b6a0ad9c41172ec207aa4de3687af1245bc9e0c0
Konrad Oleksiuk <konole@gmail.com>
2017-06-25 22:15:56 +0200

    Removes boilerplate code

--- lib/knagaroo.ex
+++ lib/knagaroo.ex
@@ -2,17 +2,4 @@ defmodule Knagaroo do
   @moduledoc """
   Documentation for Knagaroo.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Knagaroo.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
