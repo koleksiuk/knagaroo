f347cd808ca390c598a5217c82ce0424933b06ab
Konrad Oleksiuk <konole@gmail.com>
2017-06-29 23:50:44 +0200

    Remove unused docs for storage.ex

--- lib/knagaroo/storage.ex
+++ lib/knagaroo/storage.ex
@@ -1,25 +1,15 @@
 defmodule Knagaroo.Storage do
-  # === Public API
-  @doc """
-    Starts Memory storage
-  """
   @spec start_link(atom, list) :: pid
   def start_link(module, args) do
     GenServer.start_link(module, args, [])
   end
 
-  @doc """
-    Lookups storage for given key
-  """
   @spec get(pid, String.t) :: nil
   @spec get(pid, String.t) :: {:ok, String.t}
   def get(storage_pid, key) do
     GenServer.call(storage_pid, {:get, key})
   end
 
-  @doc """
-    Binds given key in a storage to a value
-  """
   @spec set(pid, String.t, String.t) :: {:ok, String.t}
   def set(storage_pid, key, value) do
     GenServer.call(storage_pid, {:set, key, value})
